13cd45a49de01bc89268d269374d0ffff5c1f402
C0D3R <jefmonva@gmail.com>
2020-09-02 16:05:14 -0500

    Restructuracion de codigo apirest

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -3,226 +3,204 @@
 namespace App\Http\Controllers;
 
 use App\Productos;
-use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class ProductosController extends Controller
 {
-
-
 
     public function consultarProductos()
     {
 
         //Consultar accesos de inicio se sesiÃ³n
-        $productos = DB::select("SELECT  p.* , e.nombre as estado, b.nombre as bodega 
+        $productos = DB::select("SELECT  p.* , e.nombre as estado, b.nombre as bodega
         			 FROM productos p
                                 inner join estados e on p.id_estado = e.id_estado
                                 inner join  bodegas b on p.id_bodega = b.id_bodega");
 
-        //Respuesta positiva
-        return response()->json([
-            'productos' => $productos //Productos
-        ], 200);
+        // Validar la información de la consulta y mostrar respuesta
+        if (!empty($productos)) {
+            $json = array(
+                "code" => 200,
+                "status" => "success",
+                "total_results" => count($productos),
+                "productos" => $productos
+            );
+        } else {
+            $json = array(
+                "code" => 404,
+                "status" => "error",
+                "total_results" => 0,
+                "message" => "Ningun registro encontrado"
+            );
+        }
+
+        return response()->json($json);
     }
+
 
     public function consultarEstados()
     {
         $estados = DB::select('SELECT  *  FROM estados e');
 
-        //Respuesta positiva
-        return response()->json([
-            'estados' => $estados //Estados
-        ], 200);
+        // Validar la información de la consulta y mostrar respuesta
+        if (!empty($estados)) {
+            $json = array(
+                "code" => 200,
+                "status" => "success",
+                "total_results" => count($estados),
+                "estados" => $estados
+            );
+        } else {
+            $json = array(
+                "code" => 404,
+                "status" => "error",
+                "total_results" => 0,
+                "message" => "Ningun registro encontrado"
+            );
+        }
+
+        return response()->json($json);
     }
 
 
     public function editarProducto(Request $request)
     {
 
-
-        $request->validate([
-            'id_producto' => 'required|integer',
-            'nombre'      => 'required|string',
-            'codigo'      => 'required|string',
-            'existencia'  => 'required|integer',
-            'bodega'      => 'nullable|string',
-            'descripcion' => 'required|string',
-            'id_estado'   => 'required|integer',
-        ]);
-
-
         $productos = Productos::findOrFail($request->id_producto);
 
-        // Inicia la transaccion
-        DB::beginTransaction();
-        try {
+        // Validar la información de la consulta y mostrar respuesta
+        if (!empty($request)) {
 
-            $productos->nombre = $request->nombre;
-            $productos->codigo = $request->codigo;
-            $productos->existencia = $request->existencia;
-            $productos->id_bodega = 1;
-            $productos->descripcion = $request->descripcion;
-            $productos->id_estado = $request->id_estado;
-
-            $productos->save();
-
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollBack();
-
-            /*
-            return response()->json([
-                'message' => 'Error!',
-                'errors' => (object) [
-                    'Error' => [
-                        $e->getMessage()
-                    ]
+            $validator = Validator::make(
+                $request->all(),
+                [
+                    'id_producto' => 'required|integer',
+                    'nombre'      => 'required|string',
+                    'codigo'      => 'required|string',
+                    'existencia'  => 'required|integer',
+                    'bodega'      => 'nullable|string',
+                    'descripcion' => 'required|string',
+                    'id_estado'   => 'required|integer',
                 ]
-            ], 422);
-            */
-        } //FINALIZA Inicia la transacción
+            );
+
+            if ($validator->fails()) {
+
+                $errors = $validator->errors();
+
+                $json = array(
+                    "code" => 400,
+                    "status" => "error",
+                    "errors" => $errors,
+                    "message" => "El producto no se ha guardado"
+                );
+            } else {
+
+                // Inicia la transaccion para guardar datos
+                DB::beginTransaction();
+                try {
+
+                    $productos->nombre = $request->nombre;
+                    $productos->codigo = $request->codigo;
+                    $productos->existencia = $request->existencia;
+                    $productos->id_bodega = 1;
+                    $productos->descripcion = $request->descripcion;
+                    $productos->id_estado = $request->id_estado;
+
+                    $productos->save();
 
 
+                    $json = array(
+                        "code" => 200,
+                        "status" => "success",
+                        "message" => "Datos actualizados corectamente",
+                        "producto" => $productos
+                    );
 
+                    DB::commit();
+                } catch (\Exception $e) {
+                    DB::rollBack();
 
-        return response()->json([
-            'msg' => 'Su producto a sido editado',
-        ], 200);
+                    $json = array(
+                        "code" => 422,
+                        "status" => "error",
+                        "errors" => $e->getMessage(),
+                    );
+                } //FINALIZA Inicia la transacción
+            }
+        }
+
+        return response()->json($json);
     }
 
     public function crearProducto(Request $request)
     {
 
+        // Validar la información de la consulta y mostrar respuesta
+        if (!empty($request)) {
 
-        $request->validate([
-            'nombre'      => 'required|string',
-            'codigo'      => 'required|string',
-            'existencia'  => 'required|integer',
-            'bodega'      => 'nullable|string',
-            'descripcion' => 'required|string',
-            'id_estado'   => 'required|integer',
-        ]);
-
-
-
-        DB::beginTransaction();
-        try {
-
-            $productos = new Productos();
-
-            $productos->nombre = $request->nombre;
-            $productos->codigo = $request->codigo;
-            $productos->existencia = $request->existencia;
-            $productos->id_bodega = 1;
-            $productos->descripcion = $request->descripcion;
-            $productos->id_estado = $request->id_estado;
-
-
-            $productos->save();
-
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollBack();
-
-            var_dump($e);
-
-            /*
-            return response()->json([
-                'message' => 'Error!',
-                'errors' => (object) [
-                    'Error' => [
-                        $e->getMessage()
-                    ]
+            $validator = Validator::make(
+                $request->all(),
+                [
+                    'nombre'      => 'required|string',
+                    'codigo'      => 'required|string',
+                    'existencia'  => 'required|integer',
+                    'bodega'      => 'nullable|string',
+                    'descripcion' => 'required|string',
+                    'id_estado'   => 'required|integer',
                 ]
-            ], 422);
-            */
-        } //FINALIZA Inicia la transacción
+            );
 
-        return response()->json(
-            [
-                'msg' => 'Su producto a sido Guardado',
-            ],
-            200
-        );
-    }
+            if ($validator->fails()) {
+
+                $errors = $validator->errors();
+
+                $json = array(
+                    "code" => 400,
+                    "status" => "error",
+                    "errors" => $errors,
+                    "message" => "El producto no se ha guardado"
+                );
+            } else {
+
+                // Inicia la transaccion para guardar datos
+                DB::beginTransaction();
+                try {
+
+                    $productos = new Productos();
+
+                    $productos->nombre = $request->nombre;
+                    $productos->codigo = $request->codigo;
+                    $productos->existencia = $request->existencia;
+                    $productos->id_bodega = 1;
+                    $productos->descripcion = $request->descripcion;
+                    $productos->id_estado = $request->id_estado;
+
+                    $productos->save();
 
 
+                    $json = array(
+                        "code" => 200,
+                        "status" => "success",
+                        "message" => "Datos guardados corectamente",
+                        "producto" => $productos
+                    );
 
+                    DB::commit();
+                } catch (\Exception $e) {
+                    DB::rollBack();
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-    }
+                    $json = array(
+                        "code" => 422,
+                        "status" => "error",
+                        "errors" => $e->getMessage(),
+                    );
+                } //FINALIZA Inicia la transacción
+            }
+        }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Productos  $productos
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Productos $productos)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Productos  $productos
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Productos $productos)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Productos  $productos
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Productos $productos)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Productos  $productos
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Productos $productos)
-    {
-        //
+        return response()->json($json);
     }
 }
